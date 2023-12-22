8a1e2b94546be67715b185ad5dca7812fc98dc6f
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 15:13:24 +0000

    Fri, Dec 22, 2023, 3:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,6 +3,25 @@
 
 int main(void)
 {
-    long n;
-    
+    long n = get_long ("Number: ");
+
+    // check length
+    int len;
+    long temp = n;
+    while (temp > 0)
+    {
+        temp = temp / 10;
+        len ++;
+    }
+    if (len != 13 || len != 15 || len != 16)
+    {
+        printf("INVALID");
+        return 0;
+    }
+
+    // check sum
+    if (checksum(n))
+    {
+        printf("VALID");
+    }
 }
