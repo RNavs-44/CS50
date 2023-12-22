cc83c7183c3590c17b9f5d09bd0e10b9d1221f12
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 16:11:19 +0000

    Fri, Dec 22, 2023, 4:11 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,14 +4,20 @@
 int main(void)
 {
     string text = get_string("Text: ");
-    int letters = 0, sentences = 0, words = 0;
+    int letters = 0, sentences = 0, words = 1;
     for (int i = 0, len = strlen(text); i < len; i++)
     {
-        if (isaplha(text[i])
+        char c = text[i];
+        if (isaplha(c)
         {
             letters++;
         }
-        
+        else if (isspace(c))
+        {
+            words++;
+        }
+        else if (c == '!' || c == '.' || c == '?')
     }
+
     int index = 0.0588 * L - 0.296 * S - 15.8;
 }
