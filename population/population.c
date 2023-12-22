10cedcb99b6a5081cf90dbc7b434163152017533
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 11:53:12 +0000

    Fri, Dec 22, 2023, 11:53 AM +00:00

---
+++ population/population.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do {
+        start = get_int("Enter starting population size: ");
+    }
+    while (start < 9);
+
+    // TODO: Prompt for end size
+    int end;
+    do {
+        end = get_int("Enter ending population size: ");
+    }
+    while (end < start);
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
