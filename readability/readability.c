830a4019a846cd5814a97ac62e7e2f4ba9a45b73
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 16:06:27 +0000

    Fri, Dec 22, 2023, 4:06 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,7 +7,7 @@ int main(void)
     int letters = 0, sentences = 0, words = 0;
     for (int i = 0, len = strlen(text); i < len; i++)
     {
-
+        if ()
     }
     int index = 0.0588 * L - 0.296 * S - 15.8;
 }
