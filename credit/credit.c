070bd953c654298558946ec5e83ca357f755ac7e
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 15:17:07 +0000

    Fri, Dec 22, 2023, 3:17 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+/*
+Luhn's Algorithm:
+1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+2. Add the sum to the sum of the digits that weren’t multiplied by 2.
+3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+*/
+
+bool checksum(long x)
+{
+    int sum1 = 0, sum2 = 0, total = 0;
+}
+
 int main(void)
 {
     long n = get_long ("Number: ");
