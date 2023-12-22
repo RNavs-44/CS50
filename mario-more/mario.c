48e7e9694c59ecc6041601b198f2316aad34a737
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-22 14:17:59 +0000

    Fri, Dec 22, 2023, 2:17 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -8,5 +8,18 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height < 1 || height > 8)
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= (height-i); j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n")
+    }
 }
