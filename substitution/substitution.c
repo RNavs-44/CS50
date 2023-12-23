96e589302399a852dc5884e55cc79cb58aa3935e
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-23 10:51:15 +0000

    Sat, Dec 23, 2023, 10:51 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -11,13 +11,31 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    // validate key consists of 26 characters
+    if (strlen(key) != 26)
+    {
+        printf("Key must contain 26 characters");
+        return 1;
+    }
+
     // validate the key consists of only alphabets
     string key = argv[1];
     for (int i = 0, n = strlen(key); i < n; i++)
     {
         if (!isalpha(key[i]))
         {
-            
+            printf("Key must contain 26 characters");
+            return 1;
+        }
+    }
+
+    // validate key consists of only unique alphabets
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            printf("Key must contain 26 characters");
+            return 1;
         }
     }
 }
