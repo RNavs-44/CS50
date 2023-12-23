1784fd08a68ab65ea3c02faf7e75b050460f384c
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-23 10:48:26 +0000

    Sat, Dec 23, 2023, 10:48 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,11 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    if (argc != 1)
+    // check only 1 command line argument
+    if (argc != 2)
     {
-        printf("ERROR");
+        printf("Usage: ./substitution key");
         return 1;
+    }
+
+    // validate the key consists of only alphabets
+    string key = argv[1];
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            
+        }
     }
 }
