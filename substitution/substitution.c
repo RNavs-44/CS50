48f76049b0298658b765f430784cd05d3980ff0b
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-23 10:59:23 +0000

    Sat, Dec 23, 2023, 10:59 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,32 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     // check only 1 command line argument
     if (argc != 2)
     {
-        printf("Usage: ./substitution key");
+        printf("Usage: ./substitution key\n");
         return 1;
     }
 
     // validate key consists of 26 characters
+    string key = argv[1];
     if (strlen(key) != 26)
     {
-        printf("Key must contain 26 characters");
+        printf("Key must contain 26 characters\n");
         return 1;
-    }
-
-    // validate the key consists of only alphabets
-    string key = argv[1];
-    for (int i = 0, n = strlen(key); i < n; i++)
-    {
-        if (!isalpha(key[i]))
-        {
-            printf("Key must contain 26 characters");
-            return 1;
-        }
     }
 
     // validate key consists of only unique alphabets
@@ -34,8 +25,32 @@ int main(int argc, string argv[])
     {
         if (!isalpha(key[i]))
         {
-            printf("Key must contain 26 characters");
+            printf("Key must contain 26 characters\n");
             return 1;
         }
+        for (int j = i + 1; j < n; j++)
+        {
+            if (key[i] == key[j])
+            {
+                printf("Key must contain 26 unique characters\n");
+                return 1;
+            }
+        }
     }
+
+    string plaintext = get_string("plaintext: ");
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        char c = plaintext[i];
+        if (isupper(c))
+        {
+            plaintext[i] = toupper(key[c-'A']);
+        }
+        else if (islower(c))
+        {
+            plaintext[i] = toupper(key[c-'a']);
+        }
+    }
+
+    printf("ciphertext: %s\n", plaintext);
 }
