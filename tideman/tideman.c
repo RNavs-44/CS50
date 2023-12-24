4b89df23c6f91da800192f0d2d6507ec04b689a0
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-24 12:58:38 +0000

    Sun, Dec 24, 2023, 12:58 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -130,15 +130,22 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
-    pair preferred;
+    pair_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
-                preferred.winner = i;
-                preferred.loser = j;
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
             }
         }
     }
