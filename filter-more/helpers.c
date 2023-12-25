e5ee40912ae148b58170083fc4312fd632efb7ab
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-25 13:36:21 +0000

    Mon, Dec 25, 2023, 1:36 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,8 +1,16 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            int sum = round((image[row][column].rgbtRed + image[row][column].rgbtBlue + image[row][column].rgbtGreen) / 3)
+        }
+    }
     return;
 }
 
