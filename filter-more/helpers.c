d87f549330a4a0ccbe84dfd3fad04afd05470041
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-25 14:25:27 +0000

    Mon, Dec 25, 2023, 2:25 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -86,5 +86,44 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
         {-2, 0, 2},
         {-1, 0, 1}
     };
+    int Gy[3][3] = {
+        {-1, -2, -1},
+        {0, 0, 0},
+        {1, 2, 1}
+    };
+    RGBTRIPLE temp[height][width];
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            int count = 0;
+            float totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int rowCoords[] = { row - 1, row, row + 1 };
+            int columnCoords[] = { column - 1, column, column + 1 };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (rowCoords[i] >= 0 && rowCoords[i] < height && columnCoords[j] >= 0 && columnCoords[j] < width)
+                    {
+                        GxRed += image[rowCoords[i]][columnCoords[j]].rgbtRed;
+                        totalGreen += image[rowCoords[i]][columnCoords[j]].rgbtGreen;
+                        totalBlue += image[rowCoords[i]][columnCoords[j]].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+            temp[row][column].rgbtRed = round(totalRed / count);
+            temp[row][column].rgbtGreen = round(totalGreen / count);
+            temp[row][column].rgbtBlue = round(totalBlue / count);
+        }
+    }
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            image[row][column] = temp[row][column];
+        }
+    }
     return;
 }
