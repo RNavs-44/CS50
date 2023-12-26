69b93e751c92c7c0c7aefc205395aeb120a8f679
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-26 10:25:06 +0000

    Tue, Dec 26, 2023, 10:25 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,10 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            
+        }
 
     }
 }
