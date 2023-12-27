b85a6734ddd9877ed14acfb35cc6ff2ad64f563d
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-27 12:47:36 +0000

    Wed, Dec 27, 2023, 12:47 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-ans = input()
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
 if ans == "42" or ans == "forty two" or ans == "forty-two":
     print("Yes")
 else:
