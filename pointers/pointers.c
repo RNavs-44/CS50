7e355ea1f831dce9a08eab4de1c1113fb085c4ee
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-28 13:05:46 +0000

    Thu, Dec 28, 2023, 1:05 PM +00:00

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -6,20 +6,8 @@
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%p\n", p); // prints location in memory
-    printf("%i\n", *p); // prints 50
-
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
-
-    char *s2 = "HI!";
-    printf("%s\n", s2);
-
-    
+    char *s = "HI!";
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
 }
