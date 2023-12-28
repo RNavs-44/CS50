a60a46a0376e1eb5c2cfc487c7c9f866f0cbe045
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-28 13:00:22 +0000

    Thu, Dec 28, 2023, 1:00 PM +00:00

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,6 +1,7 @@
 // & - provides address of something stored in memory
 // * - instructs compiler to go to a location in memory
 
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
@@ -9,4 +10,13 @@ int main(void)
     int *p = &n;
     printf("%p\n", p); // prints location in memory
     printf("%i\n", *p); // prints 50
+
+    string s = "HI!";
+    printf("%p\n", s);
+    char *q = &s[2];
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    printf("%c\n", *q);
 }
