3cb5f1f5a61daeab4d0cecf36ead0dc17b4f8935
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-28 13:53:46 +0000

    Thu, Dec 28, 2023, 1:53 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -29,7 +29,12 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    int sum = 0;
+    for (int j = 0; word[j] != '\0'; j++)
+    {
+        sum += str[j];
+    }
+    return sum % HASH_MAX;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
