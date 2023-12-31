2032d4de77c1bc34fe77059baf4895fe05f04218
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2023-12-31 18:36:31 +0000

    Sun, Dec 31, 2023, 6:36 PM +00:00

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -12,7 +12,8 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    Or(AKnight, AKnave)
+    And(Or(AKnight, AKnave), And(AKnight, AKnave))
+    Implication()
     # TODO
 )
 
