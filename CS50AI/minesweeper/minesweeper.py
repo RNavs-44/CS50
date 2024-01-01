0ff625968c8454510f7ae25bc34e295fbbf15ac0
Arnav Sharma <139798568+RNavs-44@users.noreply.github.com>
2024-01-01 14:08:18 +0000

    Mon, Jan 1, 2024, 2:08 PM +00:00

--- CS50AI/minesweeper/minesweeper.py
+++ CS50AI/minesweeper/minesweeper.py
@@ -192,7 +192,8 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        
+        self.moves_made.add(cell)
+        self
         raise NotImplementedError
 
     def make_safe_move(self):
